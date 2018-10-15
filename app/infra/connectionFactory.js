e206192620933d83c11c3988ff9b7b65354fce77
Richard <rra_ncd@hotmail.com>
2018-10-15 16:29:04 -0300

    updated tests with a base for test.

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -1,11 +1,23 @@
 var mysql = require('mysql');
 function createDbConnection() {
-    return  mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'node_js'
-    });
+    if(!process.env.NODE_ENV) {
+        return  mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'node_js'
+        });
+    }
+
+    if(process.env.NODE_ENV == 'test') {
+        return  mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'node_js_test'
+        });
+    }
+
 }
 
 module.exports = function () {
--- test/produtos.js
+++ test/produtos.js
@@ -1,11 +1,41 @@
 var express = require('../config/express')();
 var request = require('supertest')(express);
 
+beforeEach(function(done){
+    var conn = express.infra.connectionFactory();
+    conn.query("delete from produtos", function(ex,result){
+        if(!ex) {
+            done();
+        } else {
+            console.log(ex);
+        }
+    });
+});
+
 describe('#ProdutosController', function(){
     it('#listagem json', function(endFunction){
         request.get('/produtos')
             .set('Accept','application/json')
             .expect('Content-Type',/json/)
+            .expect(200, endFunction);
+    });
+
+    it('#cadastro de novo produto com dados invalidos', function(endFunction){
+        request.post('/produtos')
+            .send({name: "", value: "5", count: 2})
+            .expect(400, endFunction);
+    });
+
+    it('#cadastro de novo produto com dados validos', function(endFunction){
+        request.post('/produtos')
+            .send({name: "novo via test", value: "1", count: 2})
+            .expect(302, endFunction);
+    });
+
+    it('#cadastro de novo produto com dados validos(JSON)', function(endFunction){
+        request.post('/produtos')
+            .set('Accept','application/json')
+            .send({name: "novo via test", value: "1", count: 2})
             .expect(200, endFunction);
     });
 });
